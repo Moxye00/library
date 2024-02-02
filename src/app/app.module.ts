7f0cb23667805852e498bb17896749ad9ffed48f
moxye00 <moxanns@gmail.com>
2024-02-02 15:48:19 +0100

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { BooksDetailComponent } from './books-detail/books-detail.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BooksDetailComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
