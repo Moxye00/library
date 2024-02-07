275356e22ffcf09512bc5cc55b7cd06c23606e7e
Fabio Manna <fabiomn95@hotmail.com>
2024-02-07 17:57:06 +0100

    implementazione della scomparsa del user

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/services/authservice.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  templateUrl: 'navbar.component.html',
 })
-export class NavbarComponent {
-  constructor(private authService: AuthService) {}
+export class NavbarComponent implements OnDestroy {
+  isLoggedIn: boolean = false;
+  private userSubscription: Subscription | undefined;
 
-  isLoggedIn(): boolean {
-    return this.authService.isLoggedIn(); // Metodo per verificare lo stato di autenticazione
+  constructor(private authService: AuthService) {
+    this.userSubscription = this.authService.userPublisher.subscribe((user) => {
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSubscription?.unsubscribe();
   }
 }
