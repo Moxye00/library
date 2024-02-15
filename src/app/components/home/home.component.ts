0207eab5eeef7e5d24f0a3f7b534b1a8871c5b9b
Marco Suriano <marcosuriano1@gmail.com>
2024-02-15 11:03:08 +0100

    Update footer and home page images and links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('slideTrack', { static: false }) slideTrack: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    timer(0, 3000).subscribe(() => this.rotateCarousel());
+  }
+
+  rotateCarousel() {
+    if (this.slideTrack) {
+      const track = this.slideTrack.nativeElement;
+      const firstSlide = track.children[0];
+      const width = firstSlide.offsetWidth;
+
+      track.style.transition = 'margin-left 1s';
+      track.style.marginLeft = -width + 'px';
+
+      setTimeout(() => {
+        track.style.transition = '';
+        track.style.marginLeft = '0px';
+        track.appendChild(firstSlide);
+      }, 1000);
+    }
+  }
 
 }
